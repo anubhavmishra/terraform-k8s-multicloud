eb0ee4ada8b3223107a5d4ed283e6ad75890cf07
Anubhav Mishra <anubhavmishra@me.com>
2018-08-31 18:27:05 -0700

    add an output to configure client for aks

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "gke_kube_config" {
     value = "gcloud container clusters get-credentials --zone=${var.gke_zone} ${google_container_cluster.cluster.name}"
 }
+
+output "aks_kube_config" {
+    value = "az aks get-credentials --resource-group ${azurerm_resource_group.default.name} --name ${azurerm_kubernetes_cluster.cluster.name}"
+}
